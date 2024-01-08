b4c9609d4f872f88f87864e7b0166248265dfc0b
EdsonCuevas <efelix.cuevas@gmail.com>
2024-01-07 19:24:36 -0600

    Icons

--- src/pages/base/AsideIzquierdo/AsideIzquierdo.jsx
+++ src/pages/base/AsideIzquierdo/AsideIzquierdo.jsx
@@ -1,5 +1,7 @@
 import React, { Children } from 'react';
 import { Link } from 'react-router-dom';
+import { HiHome } from 'react-icons/hi';
+import { BiSearch } from 'react-icons/bi'
 
 const AsideIzquierdo = () => {
   return (
@@ -10,8 +12,14 @@ const AsideIzquierdo = () => {
 
         <box className="bg-neutral-800 rounded-lg h-fit w-full text-white">
           <div className='flex flex-col gap-y-4 px-5 py-4'>
-            <Link className="flex flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1">Buscar</Link>
-            <Link className="flex flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1">Search</Link>
+            <div className='flex flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1'>
+              <HiHome />
+              <Link>Home</Link>
+            </div>
+            <div className='flex flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1'>
+              <BiSearch />
+              <Link>Search</Link>
+            </div>
           </div>
         </box>
         <box className="bg-neutral-800 rounded-lg overflow-y-auto h-[100vh] text-white">
