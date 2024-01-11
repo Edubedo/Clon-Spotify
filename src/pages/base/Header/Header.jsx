e93e5a50e85f1187d6dc67bff812a53b8f2197ba
Eduardo Escobedo <ezescobedo27@gmail.com>
2024-01-10 22:51:17 -0600

    Header

--- src/pages/base/Header/Header.jsx
+++ src/pages/base/Header/Header.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsThreeDots } from 'react-icons/bs';
 import CardDesplegadaHeader from './components/CardDesplegadaHeader';
 
 function Header() {
+  const [openButtonOptions, setOpenButtonOptions] = useState(false);
   return (
     <header className="bg-[#000] pt-1 pl-3 text-[16px] sticky top-0">
-      <button name="Boton para desplegar cada  de ayuda">
+      <button
+        name="Boton para desplegar cada  de ayuda"
+        onClick={() => {
+          setOpenButtonOptions(!openButtonOptions);
+        }}
+      >
         <BsThreeDots className="text-white" />
       </button>
-      <CardDesplegadaHeader/>
+      {openButtonOptions && <CardDesplegadaHeader />}
     </header>
   );
 }
--- src/pages/base/Header/components/CardDesplegadaHeader.jsx
+++ src/pages/base/Header/components/CardDesplegadaHeader.jsx
@@ -3,24 +3,24 @@ import { FaCaretRight } from 'react-icons/fa';
 
 function CardDesplegadaHeader() {
   return (
-    <div className="absolute w-[5rem] ml-0 bg-[#303030] border border-white border-spacing-1 p-1  text-white text-[13px] rounded-[10px]">
-      <div className="flex justify-between hover:bg-[#4b4b4b] cursor-default">
+    <div className="absolute w-[8rem] ml-0 bg-[#303030] border border-gray-500 border-spacing-1 p-1  text-white text-[13px] rounded-[3px] mt-[2px]">
+      <div className="flex justify-between hover:bg-[#4b4b4b] cursor-default pl-6">
         <span>File</span>
         <FaCaretRight />
       </div>
-      <div className="flex justify-between hover:bg-[#4b4b4b] cursor-default">
+      <div className="flex justify-between hover:bg-[#4b4b4b] cursor-default pl-6">
         <span>Edit</span>
         <FaCaretRight />
       </div>
-      <div className="flex justify-between hover:bg-[#4b4b4b] cursor-default">
+      <div className="flex justify-between hover:bg-[#4b4b4b] cursor-default pl-6">
         <span>View</span>
         <FaCaretRight />
       </div>
-      <div className="flex justify-between hover:bg-[#4b4b4b] cursor-default">
+      <div className="flex justify-between hover:bg-[#4b4b4b] cursor-default pl-6">
         <span>Playback</span>
         <FaCaretRight />
       </div>
-      <div className="flex justify-between hover:bg-[#4b4b4b] cursor-default">
+      <div className="flex justify-between hover:bg-[#4b4b4b] cursor-default pl-6">
         <span>Help</span>
         <FaCaretRight />
       </div>
