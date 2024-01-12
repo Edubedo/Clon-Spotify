0be100c24e83ba97479b24808cede64a838e3659
Eduardo Escobedo <ezescobedo27@gmail.com>
2024-01-12 16:32:50 -0600

    Inicio del dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import CardPersonalizada from './CardPersonalizada';
-import CardRecommendations from './CardRecommendations';
 import CardDashboardSongs from '../../utils/CardDashboardSongs';
 
 function Dashboard() {
- 
   return (
     // PARTE DONDE VA A IR LA CARD PERSONALIZADA
     <div className="">
-      <CardPersonalizada/>
-      <CardDashboardSongs/>
-      <CardDashboardSongs/>
-      <CardDashboardSongs/>
-      <CardDashboardSongs/>
-      <CardDashboardSongs/>
-      <CardDashboardSongs/>
-      <CardDashboardSongs/>
+      <CardPersonalizada />
+      <CardDashboardSongs
+        arrayInfoSectionRecommendation={{
+          img: '',
+          title: ''
+        }}
+        rout={'recently_played'}
+        sectionName={'Recently player'}
+        alt={'recently_played'}
+      />
     </div>
   );
 }
-
 
 export default Dashboard;
--- src/utils/CardDashboardSongs.jsx
+++ src/utils/CardDashboardSongs.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function CardDashboardSongs() {
+function CardDashboardSongs({sectionName, rout, arrayInfoSectionRecommendation, alt}) {
   return (
     <div className='text-white bg-red-200 h-[400px] mt-3'>CardDashboardSongs</div>
   )
