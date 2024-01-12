a53a7fd7ed9de98382c3cffd0ed08d6ff1d1ffef
EdsonCuevas <efelix.cuevas@gmail.com>
2024-01-12 13:12:21 -0600

    Aside Lista de canciones layout terminado

--- src/components/ui/Library.jsx
+++ src/components/ui/Library.jsx
@@ -2,6 +2,10 @@ import { VscLibrary } from "react-icons/vsc"
 import { AiOutlinePlus } from "react-icons/ai"
 import { Link } from 'react-router-dom';
 
+// Imagenes
+import imgLikedSongs from '../../assets/images/album/liked_songs.jpg';
+import imgTheDoors from '../../assets/images/album/theDoors2.jpg';
+
 const Library = () => {
 
     return (
@@ -18,12 +22,22 @@ const Library = () => {
             <div className="flex flex-col gap-y-2 mt-4 px-3">
                 <div>
                 <OneTargetMusic
-                image={''}
+                image={imgLikedSongs}
                 alt={'Liked Songs'}
                 name={'Liked Songs'}
                 tipo={'Playlist'}
                 route={'/profile/likes'}
                 />
+                <div className="h-[10px]"></div>
+                <OneTargetMusic
+                image={imgTheDoors}
+                alt={'The Doors'}
+                name={'The Doors'}
+                tipo={'Album'}
+                route={'/dashboard/playlist/the_doors_exitos/'}
+                />
+
+                
                 </div>
             </div>
         </div>
@@ -34,9 +48,9 @@ function OneTargetMusic({ image, alt, name, tipo, route }) {
     return (
       <Link
         to={route}
-        className="flex items-center h-[4rem] rounded-[10px]"
+        className="flex items-center h-[4rem] rounded-[10px] hover:bg-neutral-100/5 transition"
       >
-        <img src={image} alt={alt} className="h-[100%] w-[25%] bg-red-200" />
+        <img src={image} alt={alt} className="h-[100%] w-[15%]"/>
         <div>
           <div>
             <h3 className="px-4 font-bold text-white text-1xl p">{name}</h3>
