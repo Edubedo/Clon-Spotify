a7e9beb92094e41f11c0efef4630236ea4911274
EdsonCuevas <efelix.cuevas@gmail.com>
2024-01-11 20:41:55 -0600

    Lista de canciones

--- src/components/ui/Library.jsx
+++ src/components/ui/Library.jsx
@@ -1,5 +1,6 @@
 import { VscLibrary } from "react-icons/vsc"
 import { AiOutlinePlus } from "react-icons/ai"
+import { Link } from 'react-router-dom';
 
 const Library = () => {
 
@@ -15,10 +16,35 @@ const Library = () => {
                 <AiOutlinePlus size={20} className="text-neutral-400 cursor-pointer hover:text-white transition"/>
             </div>
             <div className="flex flex-col gap-y-2 mt-4 px-3">
-                Lista de canciones
+                <div>
+                <OneTargetMusic
+                image={''}
+                alt={'Liked Songs'}
+                name={'Liked Songs'}
+                tipo={'Playlist'}
+                route={'/profile/likes'}
+                />
+                </div>
             </div>
         </div>
     );
 }
+
+function OneTargetMusic({ image, alt, name, tipo, route }) {
+    return (
+      <Link
+        to={route}
+        className="flex items-center h-[4rem] rounded-[10px]"
+      >
+        <img src={image} alt={alt} className="h-[100%] w-[25%] bg-red-200" />
+        <div>
+          <div>
+            <h3 className="px-4 font-bold text-white text-1xl p">{name}</h3>
+            <h3 className="px-4 text-white text-1xl h3">{tipo}</h3>
+          </div>
+        </div>
+      </Link>
+    );
+  }
 
 export default Library;
