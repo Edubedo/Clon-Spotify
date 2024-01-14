f0d665e758694daaef46f82be95aa06ea4e31135
EdsonCuevas <efelix.cuevas@gmail.com>
2024-01-13 22:09:24 -0600

    route home fix

--- src/pages/base/AsideIzquierdo/AsideIzquierdo.jsx
+++ src/pages/base/AsideIzquierdo/AsideIzquierdo.jsx
@@ -7,13 +7,13 @@ function AsideIzquierdo () {
   return (
 
     
-    <div className='h-full'>
+    <div className='h-full overflow-auto'>
       <div className='sm:flex flex-col gap-y-2 bg-black h-full w-[470px] p-2'>
         <div className="w-full text-white rounded-lg bg-neutral-800 h-fit">
           <div className='flex flex-col px-5 py-4 gap-y-4'>
             <div className='flex flex-row items-center w-full h-auto py-1 font-medium transition cursor-pointer gap-x-4 text-md hover:text-white text-neutral-400'>
               <HiHome size={25}/>
-              <Link>Home</Link>
+              <Link to={"/dashboard"}>Home</Link>
             </div>
             <div className='flex flex-row items-center w-full h-auto py-1 font-medium transition cursor-pointer gap-x-4 text-md hover:text-white text-neutral-400'>
               <BiSearch size={25}/>
