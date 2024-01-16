d97bc491f4063b8336a234f90e6e18ca86902116
Eduardo Escobedo <ezescobedo27@gmail.com>
2024-01-15 19:01:02 -0600

    cabeza de las notificacion

--- src/pages/dashboard/BotonesHeader.jsx
+++ src/pages/dashboard/BotonesHeader.jsx
@@ -1,40 +1,46 @@
-import { FaAngleLeft, FaAngleRight, FaRegUserCircle } from "react-icons/fa";
-import { IoIosNotificationsOutline } from "react-icons/io";
-import { MdGroups } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { FaAngleLeft, FaAngleRight, FaRegUserCircle } from 'react-icons/fa';
+import { IoIosNotificationsOutline } from 'react-icons/io';
+import { MdGroups } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 export default function BotonesHeader() {
-    return (
-      <div>
-        <div className="flex justify-between p-2 mb-10 text-white">
-          <div className="flex gap-5">
-            <button className="p-1 scale-125 bg-[#130A32] rounded-full">
-              <FaAngleLeft className="text-[25px]  " />
-            </button>
-            <button className="p-1 scale-125 bg-[#0A051A] rounded-full">
-              <FaAngleRight className="text-[25px]   " />
-            </button>
-          </div>
-          <div className="flex gap-3">
-            <Link
-              to={'/profile/notifications'}
-              className="p-1 scale-125 bg-[#130A32] rounded-full"
-            >
-              <IoIosNotificationsOutline className="text-[30px]" />
-            </Link>
-  
-            <button className="p-1 scale-125 bg-[#130A32] rounded-full">
-              <MdGroups className="text-[30px]" />
-            </button>
-  
-            <Link
-              to={'/profile'}
-              className="p-1 scale-125 bg-[#130A32] rounded-full"
-            >
-              <FaRegUserCircle className="text-[30px]" />
-            </Link>
-          </div>
+  return (
+    <div>
+      <div className="flex justify-between p-2 mb-10 text-white">
+        <div className="flex gap-5">
+          <button className="p-1 scale-125 bg-[#000] rounded-full">
+            <FaAngleLeft
+              className="text-[25px]  "
+              onClick={() => {
+                window.history.go(-1);
+                window.history.back();
+              }}
+            />
+          </button>
+          <button className="p-1 scale-125 bg-[#000] rounded-full">
+            <FaAngleRight className="text-[25px]   " />
+          </button>
+        </div>
+        <div className="flex gap-3">
+          <Link
+            to={'/profile/notifications'}
+            className="p-1 scale-125 bg-[#000] rounded-full"
+          >
+            <IoIosNotificationsOutline className="text-[30px]" />
+          </Link>
+
+          <button className="p-1 scale-125 bg-[#000] rounded-full">
+            <MdGroups className="text-[30px]" />
+          </button>
+
+          <Link
+            to={'/profile'}
+            className="p-1 scale-125 bg-[#000] rounded-full"
+          >
+            <FaRegUserCircle className="text-[30px]" />
+          </Link>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
--- src/pages/profile/notifications/Notifications.jsx
+++ src/pages/profile/notifications/Notifications.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import BotonesHeader from '../../dashboard/BotonesHeader'
+import React from 'react';
+import BotonesHeader from '../../dashboard/BotonesHeader';
 
 function Notifications() {
   return (
-    <div>
-      <BotonesHeader/>
+    <div className="bg-[#121212] text-white p-5">
+      <BotonesHeader />
+      <h1 className="text-[2.4rem] font-bold">What's New</h1>
+      <p className="mb-3 text-gray-400">
+        The latest releases from artist, podcasts, and show you follow
+      </p>
+      <div className='flex gap-3 '>
+        <button className="p-2 bg-[#232323]  rounded-r-[20px] rounded-l-[20px]">
+          Music
+        </button>
+        <button className="p-2 bg-[#232323]  rounded-r-[20px] rounded-l-[20px]">
+          Podcast & Shows
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Notifications
+export default Notifications;
