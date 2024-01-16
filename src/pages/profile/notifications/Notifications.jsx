1545e0cba866c53e813ee9c12e127d88e4801102
Eduardo Escobedo <ezescobedo27@gmail.com>
2024-01-15 19:45:30 -0600

    Notification de las notificaciones

--- src/pages/profile/notifications/Notifications.jsx
+++ src/pages/profile/notifications/Notifications.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import BotonesHeader from '../../dashboard/BotonesHeader';
-
+import Notification from './components/Notification';
+import LexFriendman from '../../../assets/images/logos/lex_friedman.jpg'
 function Notifications() {
   return (
-    <div className="bg-[#121212] text-white p-5">
-      <BotonesHeader />
-      <h1 className="text-[2.4rem] font-bold">What's New</h1>
-      <p className="mb-3 text-gray-400">
-        The latest releases from artist, podcasts, and show you follow
-      </p>
-      <div className='flex gap-3 '>
-        <button className="p-2 bg-[#232323]  rounded-r-[20px] rounded-l-[20px]">
-          Music
-        </button>
-        <button className="p-2 bg-[#232323]  rounded-r-[20px] rounded-l-[20px]">
-          Podcast & Shows
-        </button>
+    <>
+      <div className="bg-[#121212] text-white p-5">
+        <div>
+          <BotonesHeader />
+          <h1 className="text-[2.4rem] font-bold">What's New</h1>
+          <p className="mb-3 text-gray-400">
+            The latest releases from artist, podcasts, and show you follow
+          </p>
+          <div className="flex gap-3 ">
+            <button className="p-2 bg-[#232323]  rounded-r-[20px] rounded-l-[20px]">
+              Music
+            </button>
+            <button className="p-2 bg-[#232323]  rounded-r-[20px] rounded-l-[20px]">
+              Podcast & Shows
+            </button>
+          </div>
+          <div>
+          <h3 className="text-[1.6rem] font-bold mt-5">Earlier</h3>
+          <Notification img={LexFriendman} alt={"Episodio 407 lex fridman"} title={"Guillaume Verdon: Beff Jezos, E/acc Movement, Physics, Computation & AGI | Lex Fridman Podcast #407"} artist={"Lex Fridman"} description={"Guillaume Verdon (aka Beff Jezos on Twitter) is a physicist, quantum computing researcher, and founder of e/acc (effective accelerationism) movement."} type={"Episode"} uploadDate={"1 week ago"} duration={"2hr 53 min"}/>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/pages/profile/notifications/components/Notification.jsx
+++ src/pages/profile/notifications/components/Notification.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-function Notification() {
+function Notification({img, alt, title, artist, description, type, uploadDate, duration}) {
   return (
-    <div>Notification</div>
+    <div>
+        <div>
+            <img src={img} alt={alt} />
+            <div>
+                <h4>{title}</h4>
+                <h5>{artist}</h5>
+            </div>
+        </div>
+        <p>{description}</p>
+        <div> 
+            <span>{type}</span>
+            <span>{uploadDate}</span>
+            <span>{duration}</span>
+        </div>
+        <div>
+
+        </div>
+    </div>
   )
 }
 
