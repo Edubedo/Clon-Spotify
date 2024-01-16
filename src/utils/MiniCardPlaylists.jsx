ef2f9c04f19e1a7cbc07eb61b257adbefbf5bd38
Eduardo Escobedo <ezescobedo27@gmail.com>
2024-01-15 18:38:51 -0600

    Dashboard completa, solo necesitamos cambiar los artistas e imagenes pero todo bien

--- src/utils/MiniCardPlaylists.jsx
+++ src/utils/MiniCardPlaylists.jsx
@@ -4,22 +4,22 @@ import { Link } from 'react-router-dom';
 function MiniCardPlaylists({ arrayInfoSectionRecommendation }) {
   return (
     <>
-      <div className="grid items-center grid-cols-4 gap-2">
+      <div className="grid items-center grid-cols-4 gap-3 overflow-hidden">
         {arrayInfoSectionRecommendation.map(recommendation => {
           return (
-            <div className="p-1 ">
+            <div className="">
               <Link to={recommendation.route} className="">
-                <div className="p- bg-[#181818] h-[12rem]">
-                  <div className="overflow-hidden h-[6rem] ">
+                <div className=" bg-[#181818] h-[17rem] py-1 px-3">
+                  <div className="overflow-hidden h-[12rem] mt-2 ">
                     <img
-                      className="object-cover"
+                      className="w-full h-[10rem] rounded-[1rem]"
                       src={recommendation.img}
                       alt={recommendation.alt}
                     />
                   </div>
-                  <div className=" h-30">
-                    <h4>{recommendation.title}</h4>
-                    <p className="text-[10px]">{recommendation.subtitle}</p>
+                  <div className="">
+                    <h4 className='text-[1.2rem] font-bold'>{recommendation.title}</h4>
+                    <p className="text-[.9rem] text-[#777]">{recommendation.subtitle}</p>
                   </div>
                 </div>
               </Link>
