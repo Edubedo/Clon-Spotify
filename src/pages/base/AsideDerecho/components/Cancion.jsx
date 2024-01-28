90c5ea83c0b3321ac42e5d7a636a800e577a9024
Eduardo Escobedo <ezescobedo27@gmail.com>
2024-01-27 22:13:41 -0600

    Boton delete aside derecho

--- src/pages/base/AsideDerecho/components/Cancion.jsx
+++ src/pages/base/AsideDerecho/components/Cancion.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import imgTheDoors from '../../../../assets/images/album/theDoors2.jpg'
+import React from 'react';
+import imgTheDoors from '../../../../assets/images/album/theDoors2.jpg';
+import { IoMdClose } from 'react-icons/io';
 function Cancion() {
   return (
-    <div className='p-4'>
-      <h4 className='pb-5 font-semibold text-[20px]'>The doors - Exitos</h4>
-      <img src={imgTheDoors} alt="Img album the doors" className='rounded-[5px]' />
+    <div className="p-4">
+      <div className='flex items-center justify-between p-1'>
+        <h4 className="font-semibold text-[20px]">The doors - Exitos</h4>
+        <IoMdClose  className='m-1 cursor-pointer text-gray-300 hover:bg-gray-400 rounded-[50%]'/>
+      </div>
+      <img
+        src={imgTheDoors}
+        alt="Img album the doors"
+        className="rounded-[5px]"
+      />
     </div>
-  )
+  );
 }
 
-export default Cancion
+export default Cancion;
